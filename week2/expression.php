73f0869ba3e02ce889e5ea947c2bdb2e4435456f
Nguyen Linh <jonathanrocrach2@gmail.com>
2020-03-10 09:56:51 +0700

    expression

---
+++ week2/expression.php
@@ -0,0 +1,22 @@
+ 
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Expression</title>
+    </head>
+    <body>
+        <?php
+            $gr1 = 1;
+            $gr2 = 2;
+            $gr3 = 3;
+            $avg = ($gr1 + $gr2 + $gr3) / 3;
+            print "The average is $avg";
+        ?>
+    </body>
+</html>
