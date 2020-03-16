429cb4878964a61a2e8b8920a8f597a3d428284a
Nguyen Linh <jonathanrocrach2@gmail.com>
2020-03-16 21:03:06 +0700

    week1

---
+++ week1/ex1.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <h1 style="text-align:center">This is my webpage</h1>
+        <hr />
+        <p>Name: Nguyen Le Ngoc Linh</p>
+        <hr />
+        <p>
+            Class: ICT.02 K61
+        </p>
+        <hr />
+        <p>
+            Hanoi University of Science and Technology
+        </p>
+        <hr />
+    </body>
+</html>
---
+++ week1/hyperLink.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>HyperLink</title>
+  </head>
+  <body>
+    Hello world
+    <h1><a href="./ex1.php">exercise_1.php</a></h1>
+    <h1><a href="./ex5.php">exercise_5.php</a></h1>
+    <h1><a href="./identifiers.html">identifiers.html</a></h1>
+    <h1><a href="./hyperLink.php">hyperLink.php</a></h1>
+  </body>
+</html>
