6146806c697686a813fe16d2437b20d9d8f921ab
Nguyen Linh <jonathanrocrach2@gmail.com>
2020-03-16 21:36:26 +0700

    week1 update

--- week1/ex1.php
+++ week1/ex1.php
@@ -1,16 +1,44 @@
 <html>
-    <body>
-        <h1 style="text-align:center">This is my webpage</h1>
-        <hr />
-        <p>Name: Nguyen Le Ngoc Linh</p>
-        <hr />
-        <p>
-            Class: ICT.02 K61
-        </p>
-        <hr />
-        <p>
-            Hanoi University of Science and Technology
-        </p>
-        <hr />
-    </body>
+<body style="background-color: #77777777">
+
+<h2 style="text-align:right">
+    This is a HTML practice exercise
+</h2>
+
+<hr/>
+<p style="text-align: center">
+    The heading above is aligned to the right of this page. <br/>
+    The heading above is aligned to the right of this page. <br/>
+    The heading above is aligned to the right of this page.
+<p/>
+
+<hr/>
+<p style="text-align: center">
+    <b style="text-align: left" >
+        This paragraph
+        contains a lot of lines
+        in the source code,
+        but the browser
+        ignores it.
+    </b>
+</p>
+
+<hr/>
+<p>
+    <i>
+        This paragraph
+        contains a lot of spaces
+        in the source code,
+        but the browser
+        ignores it.
+    </i>
+</p>
+
+<hr/>
+<h3>This is heading 3</h3>
+
+<hr/>
+<h5>This is heading 5</h5>
+
+</body>
 </html>
--- week1/uris.php
+++ week1/uris.php
@@ -10,6 +10,6 @@
     <h1><a href="./ex1.php">exercise_1.php</a></h1>
     <h1><a href="./ex5.php">exercise_5.php</a></h1>
     <h1><a href="./identifiers.html">identifiers.html</a></h1>
-    <h1><a href="./hyperLink.php">hyperLink.php</a></h1>
+    <h1><a href="./uris.php">hyperLink.php</a></h1>
   </body>
 </html>
