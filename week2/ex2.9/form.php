74010b17930d083af72e01a4e5191f011d4fef3d
Nguyen Linh <jonathanrocrach2@gmail.com>
2020-03-23 20:53:39 +0700

    week2

--- week2/ex2.9/form.php
+++ week2/ex2.9/form.php
@@ -12,15 +12,20 @@
         $class = $_POST['class'];
         $university = $_POST['university'];
         $gender = $_POST['gender'];
+        $gpa = $_POST['gpa'];
+
         $vehicle = $_POST['vehicle'];
+        $comment = $_POST['comment'];
 
         print("<p>Hello $userName, class $class, univerity $university</p>");
         print("<p>You are a $gender</p>");
+        print("<p>Your GPA: $gpa</p>");
         print("<p>You go to school by");
-        for($x=0; $x<count($vehicle); $x++){
-            print(" $vehicle[$x],");
+        foreach($vehicle as $items){
+            print(" $items,");
         }
         print("</p>");
+        print("Comment: $comment<br>");
     ?>
 </body>
 </html>
