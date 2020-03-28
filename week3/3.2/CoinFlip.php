8fde881045a26929454c1773a13bbe0b010308e3
Nguyen Linh <jonathanrocrach2@gmail.com>
2020-03-28 16:55:19 +0700

    update coin flip

--- week3/3.2/CoinFlip.php
+++ week3/3.2/CoinFlip.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Coin flip</title>
+    <title>Coin flip</title>
 </head>
+
 <body>
     <font size="4" color="blue">Please pick head or tails!</font>
     <form action="./HeadOrTail.php" method="post">
-        <?php 
-            print '<input type="radio" name="tail" id="tail"> Tail';
-            print '<input type="radio" name="head" id="head"> Head';
-            print '<br />'
+        <?php
+        print '<input type="radio" name="pick" value="0"> Tail';
+        print '<input type="radio" name="pick" value="1"> Head';
+        print '<br />'
         ?>
         <input type="submit" value="Submit">
         <input type="reset" value="Restart">
 
     </form>
 </body>
+
 </html>
